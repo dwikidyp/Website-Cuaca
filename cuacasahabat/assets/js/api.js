33d60521c4d786fec506ab82be5d0c3089550c92
dwikidyp <dwiki834@gmail.com>
2025-08-29 13:12:48 +0700

    All module configuration

--- cuacasahabat/assets/js/api.js
+++ cuacasahabat/assets/js/api.js
@@ -29,5 +29,14 @@ export const url = {
     },
     airPollution(lat, lon) {
         return 'http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}'
+    },
+    reverseGeo(lat, lon) {
+        return 'http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5'
+    },
+    /**
+     * @param {string} query Search query e.g.: "London", "New York"
+     */
+    geo(query) {
+        return 'http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5'
     }
 }
--- cuacasahabat/assets/js/module.js
+++ cuacasahabat/assets/js/module.js
@@ -4,3 +4,36 @@
  * 
  * 
  */
+
+'use strict';
+
+export const weekDayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+export const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+];
+
+export const getDate = function (dateUnix, timezone) {
+    const date = new Date((dateUnix + timezone) * 1000);
+    const weekDayName = weekDayNames[date.getUTCDay()];
+    const monthName = monthNames[date.getUTCMonth()];
+}
