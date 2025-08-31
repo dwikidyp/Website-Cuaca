bd8e362ddc6fd434ea2c8616443300970b53ef5d
dwikidyp <dwiki834@gmail.com>
2025-08-31 19:47:12 +0700

    All module configuration

--- cuacasahabat/assets/js/module.js
+++ cuacasahabat/assets/js/module.js
@@ -32,8 +32,53 @@ export const monthNames = [
     "Dec"
 ];
 
+/**
+ * 
+ * @param {number} dateUnix Unix date in seconds
+ * @param {number} timezone Timezone shift from UTC in seconds
+ * @returns {string} Date String. formate: "Sunday 10, Jan"
+ */
+
 export const getDate = function (dateUnix, timezone) {
     const date = new Date((dateUnix + timezone) * 1000);
     const weekDayName = weekDayNames[date.getUTCDay()];
     const monthName = monthNames[date.getUTCMonth()];
+
+    return 'S{weekDayName} ${date.getUTCDate()}, ${monthName}';
+}
+
+/**
+ * 
+ * @param {number} timeUnix Unix date in seconds
+ * @param {number} timezone Timezone shift from UTC in seconds
+ * @returns {string} Time string. formate: "HH:MM AM/PM"
+ */
+
+export const getTime = function(timeUnix, timezone) {
+    const date = new Date((timeUnix + timezone) * 1000);
+    const hours = date.getUTCHours();
+    const minutes = date.getUTCMinutes();
+    const period = hours > 12 ? "PM" : "AM";
+
+    return '${hours % 12 || 12}: ${minutes} ${period}';
+}
+
+/**
+ * 
+ * @param {number} timeUnix Unix date in seconds
+ * @param {number} timezone Timezone shift from UTC in seconds
+ * @returns {string} Time string. formate: "HH AM/PM"
+ */
+
+export const getHours = function(timeUnix, timezone) {
+    const date = new Date((timeUnix + timezone) * 1000);
+    const hours = date.getUTCHours();
+    const period = hours > 12 ? "PM" : "AM";
+
+    return '${hours % 12 || 12} ${period}';
+}
+
+export const mps_to_kmh = mps => {
+    const mph = mps * 3600;
+    return mph / 1000;
 }
