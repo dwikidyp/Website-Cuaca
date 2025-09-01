7622307cb0d90724a25c5f72461a91686f135572
dwikidyp <dwiki834@gmail.com>
2025-09-01 16:14:41 +0700

    Route configuration

--- cuacasahabat/assets/js/module.js
+++ cuacasahabat/assets/js/module.js
@@ -78,7 +78,36 @@ export const getHours = function(timeUnix, timezone) {
     return '${hours % 12 || 12} ${period}';
 }
 
+/**
+ * 
+ * @param {number} mps Metter per seconds
+ * @returns {number} Kilometer per hours
+ */
+
 export const mps_to_kmh = mps => {
     const mph = mps * 3600;
     return mph / 1000;
+}
+
+export const aqiText = {
+    1: {
+        level: "Good",
+        message: "Air quality is considered satisfactory, and air pollution poses little or no risk"
+    },
+    2: {
+        level: "Fair",
+        message: "Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution."
+    },
+    3: {
+        level: "Moderate",
+        message: "Members of sensitive groups may experience health effects. The general public is not likely to be affected."
+    },
+    4: {
+        level: "Poor",
+        message: "Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects"
+    },
+    5: {
+        level: "Very Poor",
+        message: "Health warnings of emergency conditions. The entire population is more likely to be affected."
+    }
 }
--- cuacasahabat/assets/js/route.js
+++ cuacasahabat/assets/js/route.js
@@ -4,3 +4,34 @@
  * 
  * 
  */
+
+'use strict';
+
+import { updateWeather, error404 } from "./app.js";
+
+const defaultLocation = "#/weather?Lat=51.5073219&lon=-0.1276474"
+
+const currentLocation = function () {
+
+}
+
+const searchedLocation = query => {
+
+}
+
+const routes = new Map([
+    ["/current-location", currentLocation],
+    ["/weather", searchedLocation]
+]);
+
+const checkHash = function () {
+
+}
+
+window.addEventListener("hashchange", checkHash);
+
+window.addEventListener("load", function () {
+    if (!window.location.hash) {
+        window.location.hash = "#/current-location"
+    }
+});
