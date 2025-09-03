f31b76bf780e594338eddfe4409cf6e439a2055c
dwikidyp <dwiki834@gmail.com>
2025-09-03 12:44:45 +0700

    Search Api Integration

--- cuacasahabat/assets/js/app.js
+++ cuacasahabat/assets/js/app.js
@@ -8,3 +8,35 @@
 
 import { fetchData, url } from "./api.js";
 import * as module from "./module.js";
+
+/**
+ * 
+ * @param {NodeList} elements 
+ * @param {string} eventType 
+ * @param {Function} callback 
+ */
+const addEventOnElements = function(elements, eventType, callback) {
+    for (const element of elements) element.addEventListener(eventType, callback);
+}
+
+/**
+ * Toggle search in mobile device
+ */
+const searchView = document.querySelector("[data-search-view]");
+const searchTogglers = document.querySelectorAll("[data-search-toggler]");
+
+const toggleSearch = () => searchView.classList.toggle("active");
+addEventOnElements(searchTogglers, "click", toggleSearch);
+
+/**
+ * search Integration
+ */
+const searchField = document.querySelector("[data-search-field]");
+const searchResult = document.querySelector("[data-search-result]");
+
+let searchTimeout = null;
+const searchTimeoutDuration = 500;
+
+searchField.addEventListener("input", function () {
+    searchTimeout ?? clearTimeout(searchTimeout);
+});
